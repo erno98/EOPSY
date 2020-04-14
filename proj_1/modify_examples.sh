c5922fccf15214c27bac8d7cba6526aed6ab8082
Ernest Pokropek <34246636+erno98@users.noreply.github.com>
2020-04-14 20:49:52 +0200

    Add files via upload

---
+++ proj_1/modify_examples.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# first, let's create example files.
+mkdir test
+touch "test/test1" 		# ordinary file
+touch "test/test2.txt" 	# ordinary file with extension
+touch "test/tEsT3"		# another file with different capitalization
+
+# test -l flag
+
+# test -u flag
+
+# -l and -u flag together
+
+# sed pattern test
+
+# test recursion
+# for -u flag
+
+# for -l flag
+
+# for sed pattern
+
+
